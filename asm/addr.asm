1c46d33ba6db19a0d8a3286deb3d6dc0ee04aef8
Deepan Prabhu Babu <dbabu@SFOC02RQ6DYG8WN.sea.corp.expecn.com>
2018-01-25 04:53:16 -0800

    Progress in ASM Files - Working on MacOSX - Sample addition program

--- asm/addr.asm
+++ asm/addr.asm
@@ -3,11 +3,14 @@ global _main
 
 section .data
     format: db "%d", 10, 0
+    number1: dq 1
+    number2: dq 2
 section .text
 _main:
 
     sub rsp,8
-    mov rax,20
+    mov rax,[rel number1]
+    add rax,[rel number2]
     mov rdi, format
     mov rsi, rax
     mov rax,0
